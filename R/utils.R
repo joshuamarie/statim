1d939600763e2968fc7064b10f1df03cb6f53b95
unknown <joshstat126@gmail.com>
2026-05-15 14:05:21 +0800

    Mispelling correction

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,4 @@
 `%||%` = function (x, y)
     if (is.null(x)) y else x
 
-update = S7::new_external_generic("stats", "u[date", "object")
+update = S7::new_external_generic("stats", "update", "object")
