c115be333805d159fd08e8c4b17d1f0b5d8d16bf
unknown <joshstat126@gmail.com>
2026-05-28 18:06:26 +0800

    New operator

---
+++ R/ops.R
@@ -0,0 +1,59 @@
+#' Apply a method_tidy to a making_tidy target
+#'
+#' `%<-%` registers a [method_tidy()] into the tidy registry. The
+#' left-hand side must be a [making_tidy()] call.
+#'
+#' @param lhs A `making_tidy_call` object from [making_tidy()].
+#' @param rhs A [method_tidy()] object.
+#'
+#' @return `NULL` invisibly, called for its side effects.
+#'
+#' @examples
+#' making_tidy(TTEST, x_by) %<-% method_tidy(
+#'     default = function(.x, ...) { ... },
+#'     boot = function(.x, ...) { ... }
+#' )
+#'
+#' @name modifying-assignment
+#' @export
+`%<-%` = function(lhs, rhs) {
+    if (inherits(lhs, "add_variant_call")) {
+        add_variant_register(lhs, rhs)
+    } else if (inherits(lhs, "making_tidy_call")) {
+        making_tidy_register(lhs, rhs)
+    } else {
+        cli::cli_abort(
+            "Left-hand side of {.code %<-%} must be an {.fn add_variant} or {.fn making_tidy} call."
+        )
+    }
+}
+# `%<-%` = function(lhs, rhs) {
+#     if (!inherits(lhs, "making_tidy_call")) {
+#         cli::cli_abort(
+#             "Left-hand side of {.code %<-%} must be a {.fn making_tidy} call."
+#         )
+#     }
+#     if (!S7::S7_inherits(rhs, method_tidy)) {
+#         cli::cli_abort(
+#             "Right-hand side of {.code %<-%} must be a {.cls method_tidy} object."
+#         )
+#     }
+#
+#     obj = lhs$obj
+#     model_type = lhs$model_type
+#
+#     stat_cls = attr(obj, "cls") %||% cli::cli_abort(
+#         "{.arg obj} must be a function built with {.fn HTEST_FN} or {.fn MODEL_FN}."
+#     )
+#     is_model_id_class = inherits(model_type, "S7_class") && identical(model_type@parent, model_id)
+#     is_formula_class = identical(model_type, S7::class_formula)
+#     if (!is_model_id_class && !is_formula_class) {
+#         cli::cli_abort(
+#             "{.arg model_type} must be a class inheriting from {.cls model_id}, or {.code S7::class_formula}."
+#         )
+#     }
+#
+#     key = tidy_registry_key(paste0(stat_cls, "_", model_type_name(model_type)))
+#     register_tidy[[key]] = rhs
+#     invisible(NULL)
+# }
